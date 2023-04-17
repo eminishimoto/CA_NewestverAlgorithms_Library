5a2dddbd35cda8514ee8823ead6127360a3be975
emi <emi@192.168.0.17>
2023-04-17 11:13:53 +0100

    Library(package) modefied for sorting

--- src/Library/Library.java
+++ src/Library/Library.java
@@ -8,7 +8,6 @@ import Contoroller.LibraryMenu;
 import Models.Student;
 
 import Contoroller.Readfiles;
-import Contoroller.SearchFile;
 import Models.Book;
 import Models.BorrowingList;
 import Models.Searching;
@@ -21,22 +20,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author emi
  */
-public class Library {   
+public class Library {
 
     private static Student studentId;
-      //attribute as arraylists  from Model class 
-    	private ArrayList<Book> booklist; //data need to be read from csv
-	private ArrayList<Student> studentlist;//data  read from txt or random generate?
-	private ArrayList<WaitingList> waitinglist;
-        private ArrayList<BorrowingList> borrowinglist;
-        
+    //attribute as arraylists  from Model class 
+    private ArrayList<Book> booklist; //data need to be read from csv
+    private ArrayList<Student> studentlist;//data  read from txt or random generate?
+    private ArrayList<WaitingList> waitinglist;
+    private ArrayList<BorrowingList> borrowinglist;
+
     //constructor
     public Library() {
-        this.booklist=  new ArrayList<Book>(); //= booklist;
+        this.booklist = new ArrayList<Book>(); //= booklist;
         this.studentlist = new ArrayList<Student>();//studentlist;
         this.waitinglist = new ArrayList<WaitingList>();//witinglist;
         this.borrowinglist = new ArrayList<BorrowingList>();//borrowinglist;
@@ -54,101 +54,126 @@ public class Library {
 //        String calling1 = myInput.getUserText("Which author would you like to search?");
 //               SearchFile s1 = new SearchFile(calling1);
 ////        System.out.println(s1);
-  
 
- //Read files  
+        //Read files  
         Readfiles RF = new Readfiles(); //?new Readfiles is better?
- //     System.out.println("RF.loadData()"+RF.loadData());
- //     System.out.println("RF.toString()"+RF.toString());
-         System.out.println( RF.toString());
-         
-         
+        //     System.out.println("RF.loadData()"+RF.loadData());
+        //     System.out.println("RF.toString()"+RF.toString());
+        System.out.println(RF.toString());
+
         ArrayList<Book> books = RF.loadData(); //store the Bookrecord from Readfile in books to make it work in this clas
         ArrayList<Student> students = RF.loadData2();//store the Studentrecord 
 //               SearchFile s1 = new SearchFile(calling1);         
 
-        InputUtilsLB myInput = new InputUtilsLB(); 
-        
-        System.out.println(books.get(7).getBook_title());
+        InputUtilsLB myInput = new InputUtilsLB();
+//   BorrowingList     test
+//        System.out.println(books.get(7).getBook_title());
 //        books.get(6).getAuthor_first_name();
 //
-//      
+//   BorrowingList     test   
         BorrowingList bwl = new BorrowingList();
-        bwl.borrowBook(books, students, 34, 53);
-        System.out.println(bwl.toString());
-//        
-////BorrowingList        
-//          BorrowingList BL =   new BorrowingList();
-//          System.out.println("This is borrowinglist" + BL);
-//          BL.setBookid(myBook);
-       
-          
-//            p.setName ("Tolga"); //set name for p:Programmer class
-//  p.coding ("Python"); //set constractor:coding
-//    System.out.println(p.eat(p.getName()));
-//    System.out.println("This is our first programmer " + p);//設定したattributeが表示
-//    
-//        System.out.println("p.getName()" + p.getName());
-//        
-//        p.setCompanyName("CCT");
-//        
-//       p.showProfile(p.getName());
-          
-          
-          
+        
+         System.out.println("This is empty borrowinglist :" + bwl);
          
-          Searching mySearch = new Searching();//want to use book array
+//        String register1 = bwl.borrowBook(books, students, 34, 53);
+//        
+//        System.out.println(register1);
+
+
+          
+//          BL.setBookid(myBook);
+
+        Searching mySearch = new Searching();//want to use book array
 //Binary waiting for sorting part
 //        String calling9 = myInput.getUserText("Which Title would you like to search?");
 //        String lineaResult =  mySearch.binarySearch(books, calling9);//store the result from Searching class 
 //        System.out.println("test result" +  lineaResult );
-        
-//Linear
+
+
+//Linear Studebt
+        String calling12 = myInput.getUserText("Which Student name would you like to search?");
+        String lineaResult3 = mySearch.linearSearchbyStudentname(students, calling12);//store the result from Searching class 
+        System.out.println( lineaResult3);//result
+
+        String calling13 = myInput.getUserText("Which Student id  would you like to search?");
+        String lineaResult4 = mySearch.linearSearchbyStudentId(students, calling13);
+        System.out.println( lineaResult4);//result
+//Linear Book
         String calling10 = myInput.getUserText("Which Title would you like to search?");
-        String lineaResult1 =  mySearch.linearSearchbytitle(books, calling10);//store the result from Searching class 
-                 System.out.println("test result" +  lineaResult1 );
+        String lineaResult1 = mySearch.linearSearchbytitle(books, calling10);//store the result from Searching class 
+        System.out.println( lineaResult1);//result
+
+        String calling11 = myInput.getUserText("Which Author name would you like to search?");
+        String lineaResult2 = mySearch.linearSearchbyname(books, calling11);
+        System.out.println( lineaResult2);//result
+
+    
         
-        
-        String calling11 = myInput.getUserText("Which author name would you like to search?");
-        String lineaResult2 =  mySearch.linearSearchbyname(books, calling11);
-                System.out.println("test result" +  lineaResult2 );
-                
-                
-                
-                       //list books by titles and authors     
-           Sorting bubble = new Sorting();
-       
-           Book[] bookArr = new Book[books.size()];
-       
-           for(int i = 0; i<books.size();i++){
-       
-           bookArr[i]=books.get(i);
-    }
-           bubble.bubbleSort(bookArr);
- 
-           for (Book b : bookArr) {
-                System.out.println("Title: " + b.getBook_title());
-                System.out.println(" Written By : " + b.getAuthor_first_name() + " " + b.getAuthor_last_name());
-       }
+
+
+
+//list books by titles and authors     
+        Sorting bubble = new Sorting();
+
+        Book[] bookArr = new Book[books.size()];
+
+        for (int i = 0; i < books.size(); i++) {
+
+            bookArr[i] = books.get(i);
+        }
+        bubble.bubbleSort(bookArr);
+
+        for (Book b : bookArr) {
+            System.out.println("Title: " + b.getBook_title());
+            System.out.println(" Written By : " + b.getAuthor_first_name() + " " + b.getAuthor_last_name());
+        }
+        //          ArrayList<Student> students = RF.loadData2();
+//            Sorting bubble1 = new Sorting();
+//       
+//           Student[] studentArr = new Student[students.size()];
+//       
+//           for(int i = 0; i<students.size();i++){
+//       
+//           studentArr[i]=students.get(i);
+//    }
+//           bubble.bubbleSort1(studentArr);
+// 
+//           for (Student b : studentArr) {
+//                System.out.println("Student first : " + b.getStudentFirst());
+//                System.out.println(" Student  full name : " + b.getStudentFirst() );
+//       }   
+        //      + " " + b.getStudentLast()
 
 //                   ArrayList<Student> students = RF.loadData2();
         Sorting insert = new Sorting();
-        
+
         Student[] studentArr = new Student[students.size()];
-            for (int i = 0; i < students.size(); i++) {
-                studentArr[i] = students.get(i);
-            }
-                insert.insertionSort(studentArr);
-            Student student = null;
-            for (Student s : studentArr) {
-            if(student == null || !student.getStudentFirst().equals(s.getStudentFirst())){
+        for (int i = 0; i < students.size(); i++) {
+            studentArr[i] = students.get(i);
+        }
+        insert.insertionSort(studentArr);
+        Student student = null;
+        for (Student s : studentArr) {
+            if (student == null || !student.getStudentFirst().equals(s.getStudentFirst())) {
                 System.out.println("Student first Name: " + s.getStudentFirst());
-            System.out.println("Students full Name: " + s.getStudentFirst() + " " + s.getStudentLast() );
-}
-            student = s;
+                System.out.println("Students full Name: " + s.getStudentFirst() + " " + s.getStudentLast());
             }
-       }
+            student = s;
+        }
+
+        //        for (int i = 0; i < students.size(); i++) {
+//        
+//        studentArr[i] = students.get(i);
+//  }
+//        insert.insertionSort(studentArr);
+//
+//        for (Student s : studentArr) {
+//        System.out.println("Students Name: " + s.getStudentFirst() + " " + s.getStudentLast() );
+//}
     }
+}
+
+
 //       LibraryMenu LM = new LibraryMenu() ; 
 //       System.out.println("show menu" +  LM); //? 
 //       
